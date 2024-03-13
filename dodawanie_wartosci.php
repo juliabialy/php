9950e7ca217651237e9e334741667e70330e3ea1
juliabialy <132374072+juliabialy@users.noreply.github.com>
2024-03-13 08:15:26 +0100

    Create dodawanie_wartosci.php

---
+++ dodawanie_wartosci.php
@@ -0,0 +1,12 @@
+<?php
+$polaczenie = mysqli_connect('localhost','root','','ksiegarnia');
+
+
+
+
+// $zapyt2 = "INSERT INTO klient VALUES
+// (null, 'Bialy','Julia','33-100','Tarnów','Falata','10',07345678901,123123123,'j.b@wp.pl'),
+// (null,'Nowak','Ola','12-123','Marcin','Szujski','13',12345678901,123123123,'jc.p@pl')";
+// $wynik = mysqli_query($polaczenie,$zapyt2);
+// echo "dodano do listy klientów ".mysqli_affected_rows($polaczenie)." nowcyh osób";
+?>
