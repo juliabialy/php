fa1342d632d89fdbe67d8b3b84920c036d8d8cc2
juliabialy <132374072+juliabialy@users.noreply.github.com>
2024-03-13 08:28:14 +0100

    Create formularz_skrypt.php

---
+++ formularz_skrypt.php
@@ -0,0 +1,20 @@
+<?php
+$polaczenie = mysqli_connect('localhost','root','','ksiegarnia');
+$nazwisko = $_POST['nazwisko'];
+$imie = $_POST['imie'];
+$kod = $_POST['Kod'];
+$miejscowosc = $_POST['miejscowosc'];
+$ulica = $_POST['ulica'];
+$nr_domu = $_POST['nr_domu'];
+$pesel = $_POST['pesel'];
+$nr_telefonu = ['nr_telefonu'];
+$mail = $_POST['mail'];
+
+$zapyt = "INSERT INTO klient VALUES (null,'$nazwisko','$imie','$kod','$miejscowosc','$ulica',
+'$nr_domu',$pesel,$nr_telefonu,'$mail')";
+$wynik = mysqli_query($polaczenie,$zapyt);
+echo "wstawiono ".mysqli_affected_rows($polaczenie)."rekordów.";
+
+
+
+?>
